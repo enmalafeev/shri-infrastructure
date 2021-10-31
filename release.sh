7cb16773d1a05b1bb044485ed0b109681c9547d7
enmalafeev <malafeev.en@yandex.ru>
2021-10-31 17:14:43 +0300

    refactor: refactor release.sh

--- release.sh
+++ release.sh
@@ -10,7 +10,10 @@ LOG=$(git log --pretty=format:"%h - %an, %cd : %s, %ce" --date=short ${previos_t
 summary="Release ${current_tag} from ${author_tag}"
 description="${author_tag}\n${date_tag}\nVersion: ${current_tag}"
 unique_tag="release ${current_tag}"
+
 task_url="https://api.tracker.yandex.net/v2/issues/"
+get_task_url="https://api.tracker.yandex.net/v2/issues/_search"
+updateTaskUrl="https://api.tracker.yandex.net/v2/issues/"
 
 authHeader="Authorization: OAuth ${OAuth}"
 orgHeader="X-Org-Id: ${OrganizationId}"
@@ -29,4 +32,68 @@ createStatusCode=$(curl --write-out '%{http_code}' --silent --output /dev/null -
     }'
 )
 
-echo "${summary}"
+sleep 1
+
+taskKey=$(curl --silent --location --request POST ${get_task_url} \
+    --header "${authHeader}" \
+    --header "${orgHeader}" \
+    --header "${contentType}" \
+    --data-raw '{
+        "filter": {
+            "unique": "'"${unique_tag}"'"
+        }
+    }' | jq -r '.[0].key'
+)
+
+if [ "$createStatusCode" -eq 409 ]
+then
+    echo "Cannot create ticket with the same release version"
+
+    updateStatusCode=$(curl --write-out '%{http_code}' --silent --output /dev/null --location --request PATCH \
+        "${updateTaskUrl}${taskKey}" \
+        --header "${authHeader}" \
+        --header "${orgHeader}" \
+        --header "${contentType}" \
+        --data-raw '{
+            "summary": "'"${summary}"'",
+            "description": "'"${description}"' (updated)"
+        }'
+    )
+
+    if [ "$updateStatusCode" -ne 200 ]
+    then
+        echo "Error with updating ticket ${taskKey}"
+        exit 1
+    else
+        echo "Successfully updated ticket ${taskKey}"
+    fi
+
+elif [ "$createStatusCode" -ne 201 ]
+then
+    echo "Error with creating release ticket"
+    exit 1
+else
+    echo "Successfully created ticket"
+fi
+
+echo "{\"text\": \"$(echo $gitlog | tr -d ':' | tr '\r\n' ' ')\"}" | jq > tmp.json
+
+createCommentUrl="https://api.tracker.yandex.net/v2/issues/${taskKey}/comments"
+
+createCommentStatusCode=$(curl --write-out '%{http_code}' --silent --output /dev/null --location --request POST \
+        "${createCommentUrl}" \
+        --header "${authHeader}" \
+        --header "${orgHeader}" \
+        --header "${contentType}" \
+        --data-binary @tmp.json
+)
+
+if [ "$createCommentStatusCode" -ne 201 ]
+then
+    echo "Error with creating comment with gitlog for issue ${taskKey}"
+    exit 1
+else
+    echo "Successfully created comment with gitlog for issue ${taskKey}"
+fi
+
+rm tmp.json
