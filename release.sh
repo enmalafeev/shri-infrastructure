bbc6809833f01ce06c4f61f035734d00e24abbb7
enmalafeev <malafeev.en@yandex.ru>
2021-10-31 08:59:12 +0300

    refactor: upadate release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-export LAST_RELEASE_TAG=$(git describe)
-export DIFF_BETWEEN_LAST_TAGS=$(git log $(git describe --abbrev=0 --tags $(git describe --abbrev=0)^)...$(git describe --abbrev=0))
+CURRENT_TAG=$(git tag | sort -r | head -1)
+PREVIOS_TAG=$(git tag | sort -r | head -2 | tail -1)
+DIFF_BETWEEN_LAST_TAGS=$(git log $(git describe --abbrev=0 --tags $(git describe --abbrev=0)^)...$(git describe --abbrev=0))
+CURRENT_TAG_AUTHOR=$(git show ${CURRENT_TAG} | grep Author: )
+CURRENT_TAG_DATE=$(git show ${CURRENT_TAG} | grep Date:)
+LOG=$(git log $PREVIOS_TAG..$CURRENT_TAG)
 
-echo "${DIFF_BETWEEN_LAST_TAGS}"
+echo "${CURRENT_TAG_AUTHOR}"
